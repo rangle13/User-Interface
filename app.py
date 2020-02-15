6bb0f1342b99aeb5ba93fa06d52355544d0c45e3
Rodney Angle <rangle@theangles.com>
2020-02-15 02:10:12 -0500

    Add files via upload
    Adding the startup files

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/changeTime')
+def change_time():
+    return "Returning a change in time."
+
+@app.route('/tutorial')
+def tutorial():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run()
